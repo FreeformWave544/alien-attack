5d14f7986acc1236efe695790632d317453d4758
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2026-05-29 22:13:22 +0100

    Update shop.gd

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -7,3 +7,9 @@ func _ready() -> void:
 		else:
 			child.find_child("Price").modulate = Color(0.0, 1.0, 0.0, 1.0)
 		child.find_child("Value").text = str(Global.upgrades[child.name]) if child.name in Global.upgrades else "Null"
+		child.find_child("Value").pressed.connect(upgrade.bind(child.name))
+
+func upgrade(id = ""):
+	if float($Panel/GridContainer.find_child("Price").text) <= Global.score:
+		Global.upgrades[id] += 2.0
+		_ready()
