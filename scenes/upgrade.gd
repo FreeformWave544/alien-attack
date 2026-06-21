6c25922be512efeeeff8a1270298e522f30eae7f
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2026-06-21 16:57:45 +0100

    Fine Tuned Upgrade Animation

--- scenes/upgrade.gd
+++ scenes/upgrade.gd
@@ -3,10 +3,10 @@ extends Panel
 var entered = false
 func _on_mouse_entered() -> void:
 	entered = true
-	while $Background.scale.x < 1.23 and entered:
-		$Background.scale = lerp($Background.scale, Vector2(1.25, 1.25), 0.1)
+	while $Background.scale.x < 1.15 and entered:
+		$Background.scale = lerp($Background.scale, Vector2(1.1, 1.1), 0.1)
 		await get_tree().process_frame
-	$Background.scale = Vector2(1.25, 1.25)
+	$Background.scale = Vector2(1.1, 1.1)
 	entered = false
 
 func _on_mouse_exited() -> void:
